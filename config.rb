937c5d33c8675789aea3ad8402804e8952743b6e
Vinayak Patil <vinayakpat@cybage.com>
2014-01-27 18:35:19 +0530

    Initial commit for reskinning.

--- config.rb
+++ config.rb
@@ -1,12 +1,13 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
+add_import_path "bower_components/foundation-icon-fonts"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
