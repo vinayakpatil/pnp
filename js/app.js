ffd8d8268fb7ba60ce8053ee5fb6a9d944476d0f
Vinayak Patil <vinayakpat@cybage.com>
2014-02-02 22:34:08 -0800

    Completes categories page and tweaks home page components

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+	orbit: {
+		bullets: false,
+		slide_number: false,
+		timer_speed: 5000,
+		pause_on_hover: false
+	}
+});
+
+// document ready event
+$(function() {
+    $('.slide-out-div.feedback').tabSlideOut({
+        tabHandle: '.handle', //class of the element that will become your tab
+        pathToTabImage: 'imgs/feedback-vtab.png', //path to the image for the tab //Optionally can be set using css
+        imageHeight: '131px', //height of tab image           //Optionally can be set using css
+        imageWidth: '45px', //width of tab image            //Optionally can be set using css
+        tabLocation: 'left', //side of screen where tab lives, top, right, bottom, or left
+        speed: 300, //speed of animation
+        action: 'click', //options: 'click' or 'hover', action to trigger animation
+        topPos: '195px', //position from the top/ use if tabLocation is left or right
+        leftPos: '20px', //position from left/ use if tabLocation is bottom or top
+        fixedPosition: true //options: true makes it stick(fixed position) on scroll
+    });
+    $('.slide-out-div.blog').tabSlideOut({
+        tabHandle: '.handle1', //class of the element that will become your tab
+        pathToTabImage: 'imgs/blog-vtab.png', //path to the image for the tab //Optionally can be set using css
+        imageHeight: '93px', //height of tab image           //Optionally can be set using css
+        imageWidth: '45px', //width of tab image            //Optionally can be set using css
+        tabLocation: 'left', //side of screen where tab lives, top, right, bottom, or left
+        speed: 300, //speed of animation
+        action: 'click', //options: 'click' or 'hover', action to trigger animation
+        topPos: '330px', //position from the top/ use if tabLocation is left or right
+        leftPos: '20px', //position from left/ use if tabLocation is bottom or top
+        fixedPosition: true //options: true makes it stick(fixed position) on scroll
+    });
+});
