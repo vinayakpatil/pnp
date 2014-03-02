e5944982f3d6e26404f7c1e112b5675bd4009ec5
Vinayak Patil <vinayakpat@cybage.com>
2014-03-02 12:20:22 +0530

    Updates category page, adds products and product details pages.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation({
     active_class: 'open',
-	orbit: {
-		bullets: false,
-		slide_number: false,
-		timer_speed: 5000,
-		pause_on_hover: false
-	}
+    orbit: {
+        bullets: false,
+        slide_number: false,
+        timer_speed: 5000,
+        pause_on_hover: false
+    }
 });
 
 // document ready event
@@ -38,14 +38,62 @@ $(function() {
     });
 
     // Flyout menu
-    $('.flyout').hover(function(e){
+    $('.flyout').hover(function(e) {
         $(this).toggleClass('active');
-    },function(e){
+    }, function(e) {
         $(this).toggleClass('active');
     });
 
+
+    // JCarousel
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+
+    $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+
+    $('.jcarousel').each(function() {
+        // carousel controls
+        var itemLength = $(this).find('li').length;
+        var MAX_CAROUSEL_LENGTH = 4;
+
+        // Disable previous control
+        $(this).siblings('.jcarousel-prev').addClass('disabled');
+
+        if (itemLength <= MAX_CAROUSEL_LENGTH) {
+            $(this).siblings('.jcarousel-next').addClass('disabled');
+        }
+
+    });
+
+    $('.jcarousel')
+        .on('jcarousel:animateend', function(event, carousel) {
+            var currentFirstItem = $(this).jcarousel('first');
+            var currentLastItem = $(this).jcarousel('last');
+            var length = $(this).find('li').length - 1;
+
+            if (currentFirstItem.index() === 0) {
+                $(this).siblings('.jcarousel-prev').addClass('disabled');
+            } else {
+                $(this).siblings('.jcarousel-prev').removeClass('disabled');
+            }
+
+            if (currentLastItem.index() === length) {
+                $(this).siblings('.jcarousel-next').addClass('disabled');
+            } else {
+                $(this).siblings('.jcarousel-next').removeClass('disabled');
+            }
+        });
+
     // Hack to force navigation. Need to clean this up
-    $('.flyout>a').on('click', function(e){
+    $('.flyout>a').on('click', function(e) {
         window.location = $(this).attr('href');
     });
 });
