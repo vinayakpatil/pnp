6a80a3e0c1d6205f98b97b20d7dc9b77c5f7f532
Vinayak Patil <vinayakpat@cybage.com>
2014-03-02 22:47:59 +0530

    Minor touch ups to nav-bar and product details page.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,13 @@ $(document).foundation({
 
 // document ready event
 $(function() {
+    // Hack to force navigation. Need to clean this up
+    $('.flyout>a').on('click', function(e) {
+        console.log('taeg');
+        window.location = $(this).attr('href');
+    });
+
+
     $('.slide-out-div.feedback').tabSlideOut({
         tabHandle: '.handle', //class of the element that will become your tab
         pathToTabImage: 'imgs/feedback-vtab.png', //path to the image for the tab //Optionally can be set using css
@@ -42,58 +49,5 @@ $(function() {
         $(this).toggleClass('active');
     }, function(e) {
         $(this).toggleClass('active');
-    });
-
-
-    // JCarousel
-    $('.jcarousel').jcarousel({
-        // Configuration goes here
-    });
-
-    $('.jcarousel-prev').jcarouselControl({
-        target: '-=1'
-    });
-
-    $('.jcarousel-next').jcarouselControl({
-        target: '+=1'
-    });
-
-
-    $('.jcarousel').each(function() {
-        // carousel controls
-        var itemLength = $(this).find('li').length;
-        var MAX_CAROUSEL_LENGTH = 4;
-
-        // Disable previous control
-        $(this).siblings('.jcarousel-prev').addClass('disabled');
-
-        if (itemLength <= MAX_CAROUSEL_LENGTH) {
-            $(this).siblings('.jcarousel-next').addClass('disabled');
-        }
-
-    });
-
-    $('.jcarousel')
-        .on('jcarousel:animateend', function(event, carousel) {
-            var currentFirstItem = $(this).jcarousel('first');
-            var currentLastItem = $(this).jcarousel('last');
-            var length = $(this).find('li').length - 1;
-
-            if (currentFirstItem.index() === 0) {
-                $(this).siblings('.jcarousel-prev').addClass('disabled');
-            } else {
-                $(this).siblings('.jcarousel-prev').removeClass('disabled');
-            }
-
-            if (currentLastItem.index() === length) {
-                $(this).siblings('.jcarousel-next').addClass('disabled');
-            } else {
-                $(this).siblings('.jcarousel-next').removeClass('disabled');
-            }
-        });
-
-    // Hack to force navigation. Need to clean this up
-    $('.flyout>a').on('click', function(e) {
-        window.location = $(this).attr('href');
     });
 });
---
+++ js/jcarousel.js
@@ -0,0 +1,49 @@
+$(function() {
+
+    // JCarousel
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+
+    $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });
+
+
+    $('.jcarousel').each(function() {
+        // carousel controls
+        var itemLength = $(this).find('li').length;
+        var MAX_CAROUSEL_LENGTH = 4;
+
+        // Disable previous control
+        $(this).siblings('.jcarousel-prev').addClass('disabled');
+
+        if (itemLength <= MAX_CAROUSEL_LENGTH) {
+            $(this).siblings('.jcarousel-next').addClass('disabled');
+        }
+
+    });
+
+    $('.jcarousel')
+        .on('jcarousel:animateend', function(event, carousel) {
+            var currentFirstItem = $(this).jcarousel('first');
+            var currentLastItem = $(this).jcarousel('last');
+            var length = $(this).find('li').length - 1;
+
+            if (currentFirstItem.index() === 0) {
+                $(this).siblings('.jcarousel-prev').addClass('disabled');
+            } else {
+                $(this).siblings('.jcarousel-prev').removeClass('disabled');
+            }
+
+            if (currentLastItem.index() === length) {
+                $(this).siblings('.jcarousel-next').addClass('disabled');
+            } else {
+                $(this).siblings('.jcarousel-next').removeClass('disabled');
+            }
+        });
+});
